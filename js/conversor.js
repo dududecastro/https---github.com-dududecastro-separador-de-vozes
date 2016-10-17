fb8d0c1a1c1c368ff90d0482a7b9b66be8f65df2
dududecastro <contato@dududecastro.com>
2016-10-17 01:00:49 -0200

    Transposição níve 1 (handmade)

--- js/conversor.js
+++ js/conversor.js
@@ -4,10 +4,6 @@ window.items = ['C','C#','D','D#','E','F','F#','G','G#','A','Bb','B','C','C#','D
 window.classname = document.getElementsByClassName('action');
 window.minornote = document.getElementsByClassName('notamenor');
 
-function initBody(){
-
-}
-
 function displayScale() {
 
 	window.ResultadoPrimeira = document.getElementById("notasmusicas-resultados-primeira");
@@ -70,7 +66,7 @@ function displayScale() {
 	window.quinta = scaleFinal.slice(4, 7).concat(scaleFinal.slice(0, 4));
 
 
-	document.getElementById("tecladoNotas").innerHTML = '<li>' + primeira.join("</li><li>") + '</li><li class="deleteNote">◁</li>';
+	document.getElementById("tecladoNotas").innerHTML = '<li>' + primeira.join("</li><li>") + '</li><li class="deleteNote">◄</li>';
 
 }
 
@@ -145,6 +141,9 @@ function Maior() {
 		document.getElementById("notas-resultado").innerHTML = primeira.join(" - ");
 		document.getElementById("notas-resultado2").innerHTML = terca.join(" - ");
 		document.getElementById("notas-resultado3").innerHTML = quinta.join(" - ");
+		document.querySelector(".escalas1").setAttribute('style','display:block');
+		document.querySelector(".escalas2").setAttribute('style','display:block');
+		document.querySelector(".escalas3").setAttribute('style','display:block');
 }
 function Menor() {
 	displayScale();
@@ -155,4 +154,83 @@ function Menor() {
 		document.getElementById("notas-resultado").innerHTML = primeira.join(" - ");
 		document.getElementById("notas-resultado2").innerHTML = terca.join(" - ");
 		document.getElementById("notas-resultado3").innerHTML = quinta.join(" - ");
+		document.querySelector(".escalas1").setAttribute('style','display:block');
+		document.querySelector(".escalas2").setAttribute('style','display:block');
+		document.querySelector(".escalas3").setAttribute('style','display:block');
+}
+
+function Bb1() {
+	window.notasNatural1 = document.getElementById("notasmusicas-resultados-primeira").value.split(/\s+/g);
+	ResultadoPrimeira.innerHTML = '';
+	for (var i = 0, len = notasmusica.length; i < len; i++) {
+		if(notasNatural1[i]){
+			window.notePosition1 = scaleFinal.indexOf(notasNatural1[i]);
+				if (primeira[notePosition1] === 'C') {notafinal = items[14];};
+				if (primeira[notePosition1] === 'C#' || primeira[notePosition1] === 'Db') {notafinal = items[15];};
+				if (primeira[notePosition1] === 'D') {notafinal = items[16];};
+				if (primeira[notePosition1] === 'D#' || primeira[notePosition1] === 'Eb') {notafinal = items[17];};
+				if (primeira[notePosition1] === 'E') {notafinal = items[18];};
+				if (primeira[notePosition1] === 'F') {notafinal = items[19];};
+				if (primeira[notePosition1] === 'F#' || primeira[notePosition1] === 'Gb') {notafinal = items[20];};
+				if (primeira[notePosition1] === 'G') {notafinal = items[21];};
+				if (primeira[notePosition1] === 'G#' || primeira[notePosition1] === 'Ab') {notafinal = items[22];};
+				if (primeira[notePosition1] === 'A') {notafinal = items[23];};
+				if (primeira[notePosition1] === 'A#' || primeira[notePosition1] === 'Bb') {notafinal = items[24];};
+				if (primeira[notePosition1] === 'B') {notafinal = items[25];};
+			ResultadoPrimeira.insertAdjacentHTML('beforeend', notafinal + ' ');
+		} else {
+			ResultadoPrimeira.insertAdjacentHTML('beforeend', 'Volte, e tente novamente.');
+		}
+	}
+	document.getElementById('qualvoz1Bb').setAttribute('style','display:none');
+}
+function Bb2() {
+	window.notasNatural2 = document.getElementById("notasmusicas-resultados-terca").value.split(/\s+/g);
+	ResultadoTerca.innerHTML = '';
+	for (var i = 0, len = notasmusica.length; i < len; i++) {
+		if(notasNatural2[i]){
+			window.notePosition2 = scaleFinal.indexOf(notasNatural2[i]);
+				if (primeira[notePosition2] === 'C') {notafinal = items[14];};
+				if (primeira[notePosition2] === 'C#' || primeira[notePosition2] === 'Db') {notafinal = items[15];};
+				if (primeira[notePosition2] === 'D') {notafinal = items[16];};
+				if (primeira[notePosition2] === 'D#' || primeira[notePosition2] === 'Eb') {notafinal = items[17];};
+				if (primeira[notePosition2] === 'E') {notafinal = items[18];};
+				if (primeira[notePosition2] === 'F') {notafinal = items[19];};
+				if (primeira[notePosition2] === 'F#' || primeira[notePosition2] === 'Gb') {notafinal = items[20];};
+				if (primeira[notePosition2] === 'G') {notafinal = items[21];};
+				if (primeira[notePosition2] === 'G#' || primeira[notePosition2] === 'Ab') {notafinal = items[22];};
+				if (primeira[notePosition2] === 'A') {notafinal = items[23];};
+				if (primeira[notePosition2] === 'A#' || primeira[notePosition2] === 'Bb') {notafinal = items[24];};
+				if (primeira[notePosition2] === 'B') {notafinal = items[25];};
+			ResultadoTerca.insertAdjacentHTML('beforeend', notafinal + ' ');
+		} else {
+			ResultadoTerca.insertAdjacentHTML('beforeend', 'Volte, e tente novamente.');
+		}
+	}
+	document.getElementById('qualvoz2Bb').setAttribute('style','display:none');
+}
+function Bb3() {
+	window.notasNatural3 = document.getElementById("notasmusicas-resultados-quinta").value.split(/\s+/g);
+	ResultadoQuinta.innerHTML = '';
+	for (var i = 0, len = notasmusica.length; i < len; i++) {
+		if(notasNatural3[i]){
+			window.notePosition3 = scaleFinal.indexOf(notasNatural3[i]);
+				if (primeira[notePosition3] === 'C') {notafinal = items[14];};
+				if (primeira[notePosition3] === 'C#' || primeira[notePosition3] === 'Db') {notafinal = items[15];};
+				if (primeira[notePosition3] === 'D') {notafinal = items[16];};
+				if (primeira[notePosition3] === 'D#' || primeira[notePosition3] === 'Eb') {notafinal = items[17];};
+				if (primeira[notePosition3] === 'E') {notafinal = items[18];};
+				if (primeira[notePosition3] === 'F') {notafinal = items[19];};
+				if (primeira[notePosition3] === 'F#' || primeira[notePosition3] === 'Gb') {notafinal = items[20];};
+				if (primeira[notePosition3] === 'G') {notafinal = items[21];};
+				if (primeira[notePosition3] === 'G#' || primeira[notePosition3] === 'Ab') {notafinal = items[22];};
+				if (primeira[notePosition3] === 'A') {notafinal = items[23];};
+				if (primeira[notePosition3] === 'A#' || primeira[notePosition3] === 'Bb') {notafinal = items[24];};
+				if (primeira[notePosition3] === 'B') {notafinal = items[25];};
+			ResultadoQuinta.insertAdjacentHTML('beforeend', notafinal + ' ');
+		} else {
+			ResultadoQuinta.insertAdjacentHTML('beforeend', 'Volte, e tente novamente.');
+		}
+	}
+	document.getElementById('qualvoz3Bb').setAttribute('style','display:none');
 }
